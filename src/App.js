0cedb66249d39fe436684a5a85a5137c6894c7b1
weisssean <weisssean@gmail.com>
2021-04-01 09:28:20 -0400

    removed a lot of junk

--- src/App.js
+++ src/App.js
@@ -1,45 +1,8 @@
 import { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { subscribe, disconnect } from "./utils/events";
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Paper, ThemeProvider } from "@material-ui/core";
-import { createMuiTheme } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    height: "100%",
-  },
-  iframe: {
-    width: "100%",
-    height: "100%",
-  },
-  grid: {
-    width: "100%",
-    height: "100%",
-  },
-}));
-
-const darkTheme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  },
-});
 
 function App() {
-  const classes = useStyles();
-
   const [temp, setTemp] = useState(false);
   const [humidity, setHumidity] = useState(0);
   const [pressure, setPressure] = useState(0);
@@ -57,42 +20,44 @@ function App() {
       setPressure(result.iotData.pressure);
     }
   };
-
+//http://192.168.1.178:8088/
   useEffect(() => {
+    window.document.getElementById("frm").src = "http://"+window.location.hostname + ":8088"
     subscribe(callback);
     return () => {
+      
       disconnect();
     };
   }, []);
   return (
-    <ThemeProvider theme={darkTheme}>
-    <div className="App" className={classes.root}>
-      <Grid container spacing={0} className={classes.grid}>
-        <Grid item xs={12} sm={8}>
-            <iframe
-              className={classes.iframe}
-              src="http://96.230.190.210:8099/"
-            ></iframe>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <div className={classes.paper}>
-            <Grid item xs={12}>
-              <h2>{deviceId}</h2>
-            </Grid>
-            <Grid item xs={12}>
-              <h3>{`Temperature: ${(temp * (9 / 5) + 32).toFixed(2)}°F`}</h3>
-            </Grid>
-            <Grid item xs={12}>
-              <h3>{`Hunidity: ${humidity}%`}</h3>
-            </Grid>
-            <Grid item xs={12}>
-              <h3>{`Pressure: ${pressure}`}</h3>
-            </Grid>
+    <>
+    {/* <iframe className="iframe" src="http://96.230.190.210:8099/"  scrolling="yes" height="100%" width="100%" ></iframe> */}
+    <div className="App-body">
+      <h2 className="title">Greenroom 1</h2>
+      <iframe className="iframe" id="frm"  src={`http://${window.location.hostname}:8088`}  scrolling="yes" height="100%" width="100%" ></iframe>
+      <div className="app">
+        <div class="row">
+          {/* <div class="column">
+            <h3>{deviceId}</h3>
+          </div> */}
+          <div class="column">
+            <h1>{`Temperature: ${(temp * (9 / 5) + 32).toFixed(2)}°F`}</h1>
           </div>
-        </Grid>
-      </Grid>
-    </div>
-    </ThemeProvider>
+          <div class="column">
+            <h1>{`Hunidity: ${humidity}%`}</h1>
+          </div>
+          {/* <div class="column">
+            {" "}
+            <h3>{`Pressure: ${pressure}`}</h3>
+          </div> */}
+        </div>
+        {/* <h2>{deviceId}</h2>
+        <h3>{`Temperature: ${(temp * (9 / 5) + 32).toFixed(2)}°F`}</h3>
+        <h3>{`Hunidity: ${humidity}%`}</h3>
+        <h3>{`Pressure: ${pressure}`}</h3> */}
+      </div>
+      </div>
+    </>
   );
 }
 
